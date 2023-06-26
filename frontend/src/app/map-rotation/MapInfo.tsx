9578b5662defa28341845dd9cf57305ad224e49a
k2font <shoichiro.kouno@gmail.com>
2023-06-26 19:03:56 +0900

    重複コードをコンポーネントに切り出し(MapInfo)

---
+++ frontend/src/app/map-rotation/MapInfo.tsx
@@ -0,0 +1,48 @@
+import Image from 'next/image'
+
+
+type RotationInfo = {
+  current: {
+    map: string,
+    remainingTimer: string,
+    asset: string
+  },
+  next: {
+    map: string,
+    asset: string
+  },
+}
+
+const MapInfo = ({
+  rotationInfo,
+  title
+}: {
+  rotationInfo: RotationInfo, 
+  title: string
+}) => {
+  return (
+    <>
+      <h2>{title}</h2>
+      <h3>Current Map</h3>
+      <p>{rotationInfo.current.map}</p>
+      <p>Remain: {rotationInfo.current.remainingTimer}</p>
+      <Image
+        src={rotationInfo.current.asset}
+        width={600}
+        height={250}
+        alt={rotationInfo.current.map}
+      />
+
+      <h3>Next Map</h3>
+      <p>{rotationInfo.next.map}</p>
+      <Image
+        src={rotationInfo.next.asset}
+        width={600}
+        height={250}
+        alt={rotationInfo.next.map}
+      />
+    </>
+  );
+}
+
+export default MapInfo;
--- frontend/src/app/map-rotation/page.tsx
+++ frontend/src/app/map-rotation/page.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+import MapInfo from './MapInfo';
 
 const getMapRotation = async () => {
   const res = await fetch("http://localhost:8080/map-rotation")
@@ -16,46 +16,8 @@ const MapRotation = async ({
     <section>
       <nav>
         <h1>Map Rotation</h1>
-        
-        <h2>Battle Royal</h2>
-        <h3>Current Map</h3>
-        <p>{map_rotation.battle_royale.current.map}</p>
-        <p>Remain: {map_rotation.battle_royale.current.remainingTimer}</p>
-        <Image
-          src={map_rotation.battle_royale.current.asset}
-          width={600}
-          height={250}
-          alt={map_rotation.battle_royale.current.map}
-        />
-
-        <h3>Next Map</h3>
-        <p>{map_rotation.battle_royale.next.map}</p>
-        <Image
-          src={map_rotation.battle_royale.next.asset}
-          width={600}
-          height={250}
-          alt={map_rotation.battle_royale.next.map}
-        />
-
-        <h2>Ranked</h2>
-        <h3>Current Map</h3>
-        <p>{map_rotation.ranked.current.map}</p>
-        <p>Remain: {map_rotation.ranked.current.remainingTimer}</p>
-        <Image
-          src={map_rotation.ranked.current.asset}
-          width={600}
-          height={250}
-          alt={map_rotation.ranked.current.map}
-        />
-
-        <h3>Next Map</h3>
-        <p>{map_rotation.ranked.next.map}</p>
-        <Image
-          src={map_rotation.ranked.next.asset}
-          width={600}
-          height={250}
-          alt={map_rotation.ranked.next.map}
-        />
+        <MapInfo rotationInfo={map_rotation.battle_royale} title="Battile Royale" />
+        <MapInfo rotationInfo={map_rotation.ranked} title="Ranked" />        
       </nav>
       {children}
     </section>

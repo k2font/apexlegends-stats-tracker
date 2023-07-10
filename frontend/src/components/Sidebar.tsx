e6c633635ec7a181b0b836eb0af2a77f3c1678fb
k2font <shoichiro.kouno@gmail.com>
2023-07-10 18:17:17 +0900

    サイドバーの実装

---
+++ frontend/src/components/Sidebar.tsx
@@ -0,0 +1,16 @@
+const Sidebar = () => {
+  return (
+    <>
+      <div className="sidebar">
+        <ul className="sidebarContents">
+          <li>Server Status</li>
+          <li>Map Rotation</li>
+          <li>Predator Border</li>
+          <li>Search</li>
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default Sidebar;

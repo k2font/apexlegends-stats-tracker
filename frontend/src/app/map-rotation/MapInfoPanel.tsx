43b9574034938c4462c6f5ba08025b34d661d142
k2font <shoichiro.kouno@gmail.com>
2023-07-12 17:14:06 +0900

    カウントダウンタイマーの実装

--- frontend/src/app/map-rotation/MapInfoPanel.tsx
+++ frontend/src/app/map-rotation/MapInfoPanel.tsx
@@ -1,9 +1,13 @@
+'use client'
+
+import { useState, useEffect } from 'react';
 import Image from 'next/image'
 
 type RotationInfo = {
   map: string,
   remainingTimer?: string,
-  asset: string
+  asset: string,
+  remainingSecs: number,
 }
 
 const MapInfoPanel = ({
@@ -15,12 +19,29 @@ const MapInfoPanel = ({
   title: string
   isCurrentMap: boolean
 }) => {
+  const targetSeconds = rotationInfo.remainingSecs;
+  const [timeRemaining, setTimeRemaining] = useState(targetSeconds);
+
+  // 毎秒デクリメントする処理
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimeRemaining(timeRemaining => timeRemaining - 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  // 秒形式の時刻を時間、分、秒に変換する
+  const hoursLeft = Math.floor(timeRemaining / 60 / 60);
+  const minutesLeft = Math.floor((timeRemaining / 60) % 60);
+  const secondsLeft = timeRemaining % 60;
+
   return (
     <>
       { isCurrentMap && <h2>{title}</h2> }
       <h3>{isCurrentMap ? "Current Map" : "Next Map"}</h3>
       <p>{rotationInfo.map}</p>
-      { isCurrentMap && <p>Remain: {rotationInfo.remainingTimer}</p> }
+      { isCurrentMap && <p>Remain: {hoursLeft}時間 {minutesLeft}分 {secondsLeft}秒</p> }
       <Image
         src={rotationInfo.asset}
         width={600}
